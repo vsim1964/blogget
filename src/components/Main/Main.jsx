2d375d9d8ad13fcd267ef5443d933048ce6099a6
Siminenko <vsim1964@gmail.com>
2022-09-23 17:41:49 +0300

    5 урок списки. ключи

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,29 +2,12 @@ import { Layout } from '../Layout/Layout';
 import style from './Main.module.css';
 import Tabs from './Tabs';
 import List from './List';
-import { assignId} from '../../utils/generateId';
-import { useState } from 'react';
 
-const listRubrics = [
-  {value: 'Главная'},
-  {value: 'Просмотренные'},
-  {value: 'Сохраненные'},
-  {value: 'Мои посты'},
-].map(assignId);
-
-export const Main = () => {
-  const [rubrics, setRubrics] = useState(listRubrics);
-  const addRubric = () => {
-    // setRubrics(rubrics.concat(assignId({value: 'Новая рубрика'})));
-    setRubrics([...rubrics, assignId({value: 'Новая рубрика'})]);
-  };
-  return (
-    <main>
-      <Layout classname={style.main}>
-        <button onClick={addRubric}>addRubric</button>
-        <Tabs rubrics={rubrics} setRubrics={setRubrics}/>
-        <List/>
-      </Layout>
-    </main>
-  );
-};
+export const Main = () => (
+  <main>
+    <Layout classname={style.main}>
+      <Tabs/>
+      <List/>
+    </Layout>
+  </main>
+);
--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -1,24 +1,70 @@
 import style from './Tabs.module.css';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { assignId} from '../../../utils/generateId';
+import {debounceRaf} from '../../../utils/debounce';
 
-const Tabs = ({rubrics, setRubrics}) => {
-  const [isDropdownOpen] = useState(false);
+import { ReactComponent as ArrowIcon } from './img/arrow.svg';
+import { ReactComponent as EyeIcon } from './img/eye.svg';
+import { ReactComponent as PostIcon } from './img/post.svg';
+import { ReactComponent as SaveIcon } from './img/save.svg';
+import { ReactComponent as HomeIcon } from './img/home.svg';
 
-  const handleClick = id => {
-    setRubrics(rubrics.filter(item => item.id !== id));
+const listRubrics = [
+  {value: 'Главная', Icon: HomeIcon},
+  {value: 'Просмотренные', Icon: EyeIcon},
+  {value: 'Сохраненные', Icon: SaveIcon},
+  {value: 'Мои посты', Icon: PostIcon},
+].map(assignId);
+
+const Tabs = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isDropdown, setIsDropDown] = useState(true);
+
+  const handleResize = () => {
+    if (document.documentElement.clientWidth < 768) {
+      setIsDropDown(true);
+    } else {
+      setIsDropDown(false);
+    }
   };
 
+  useEffect(() => {
+    const debounceResize = debounceRaf(handleResize);
+    debounceResize();
+    window.addEventListener('resize', debounceResize);
+    return () => {
+      window.removeEventListener('resize', debounceResize);
+    };
+  }, []);
+
   return (
-    <>
-      {isDropdownOpen && <ul className={style.rubrics}>
-        {rubrics.map(({value, id}) => (
-          <li key={id}>
-            <button onClick={() => handleClick(id)}>{value}</button>
-          </li>
-        ))}
-      </ul>}
-    </>
+    <div className={style.container}>
+      {isDropdown && (
+        <div className={style.wrapperBtn}>
+          <button
+            className={style.btn}
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+          Открыть меню
+            <ArrowIcon width={15} heigyn={15}/>
+          </button>
+        </div>
+      )}
+
+      {(isDropdownOpen || !isDropdown) && (
+        <ul className={style.rubrics} onClick={() => setIsDropdownOpen(false)}>
+          {listRubrics.map(({value, id, Icon}) => (
+            <li className={style.item} key={id}>
+              <button
+                className={style.btn}
+                onClick={() => {}}>
+                {value}
+                {Icon && <Icon width={25} height={25} />}
+              </button>
+            </li>))}
+        </ul>
+      )}
+    </div>
   );
 };
 export default Tabs;
